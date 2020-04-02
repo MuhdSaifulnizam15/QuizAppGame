2805b565fada448abb4d33eedab675637a9e2c67
Muhd Saifulnizam <muhdsaifulnizam1998@gmail.com>
2020-04-03 02:32:55 +0800

    added new questions from open trivia db via fetch api

--- game.js
+++ game.js
@@ -13,13 +13,29 @@ let availableQuestions = [];
 
 let questions = [];
 
-fetch("questions.json")
+fetch(
+    "https://opentdb.com/api.php?amount=10&category=9&difficulty=easy&type=multiple"
+)
     .then( res => {
         console.log(res);
         return res.json();
     }).then( loadedQuestions => {
-        console.log(loadedQuestions);
-        question = loadedQuestions;
+        console.log(loadedQuestions.results);
+        questions = loadedQuestions.results.map( loadedQuestions => {
+            const formattedQuestion = {
+                question: loadedQuestions.question
+            };
+
+            const answerChoices = [ ... loadedQuestions.incorrect_answers];
+            formattedQuestion.answer = Math.floor(Math.random()*3) + 1;
+            answerChoices.splice(formattedQuestion.answer -1, 0, loadedQuestions.correct_answer);
+
+            answerChoices.forEach((choice, index) => {
+                formattedQuestion["choice" + (index+1)] = choice
+            })
+
+            return formattedQuestion;
+        });
         startGame();
     })
     .catch( err => {
@@ -28,7 +44,7 @@ fetch("questions.json")
 
 // CONSTANTS
 const CORRECT_BONUS = 10;
-const MAX_QUESTIONS = 3;
+const MAX_QUESTIONS = 10;
 
 
 startGame = () => {
